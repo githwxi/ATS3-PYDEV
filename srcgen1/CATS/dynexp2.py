e2e26bcf14576271560f2ca234c80fd9c5d1317c
Hongwei <gmhwxi@gmail.com>
2026-06-06 16:52:05 -0400

    Updating: very very minorly

--- srcgen1/CATS/dynexp2.py
+++ srcgen1/CATS/dynexp2.py
@@ -16,11 +16,86 @@ type d2exp = D2E000 \
 class D2E000(ABC):
     pass
 ######
+class D2Eint(D2E000):
+    """
+    HX: for signed ints
+    """
+    ctag = "D2Eint"
+    pass
+class D2Ebtf(D2E000):
+    """
+    HX: for true / false
+    """
+    ctag = "D2Ebtf"
+    pass
+class D2Echr(D2E000):
+    """
+    HX: for chars(==int8)
+    """
+    ctag = "D2Echr"
+    pass
+class D2Eflt(D2E000):
+    """
+    HX: for double floats
+    """
+    ctag = "D2Eflt"
+    pass
+class D2Estr(D2E000):
+    """
+    HX: for constant strings
+    """
+    ctag = "D2Estr"
+    pass
+######
+class D2Ei00(D2E000):
+    """
+    HX: for macdef ints
+    """
+    ctag = "D2Ei00"
+    pass
+class D2Eb00(D2E000):
+    """
+    HX: for macdef bools
+    """
+    ctag = "D2Eb00"
+    pass
+class D2Ec00(D2E000):
+    """
+    HX: for macdef chars
+    """
+    ctag = "D2Ec00"
+    pass
+class D2Ef00(D2E000):
+    """
+    HX: for macdef floats
+    """
+    ctag = "D2Ef00"
+    pass
+class D2Es00(D2E000):
+    """
+    HX: for static strings
+    """
+    ctag = "D2Es00"
+    pass
+######
 class D2Econ(D2E000):
+    """
+    HX: for data constructors
+    """
+    ctag = "D2Econ"
     pass
 class D2Ecst(D2E000):
+    """
+    HX: for dynamic constants
+    HX: (values and functions)
+    """
+    ctag = "D2Ecst"
     pass
 class D2Evar(D2E000):
+    """
+    HX: for dynamic variables
+    """
+    ctag = "D2Evar"
     pass
 ########################################################################
 ########################################################################
