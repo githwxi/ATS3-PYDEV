a320ad0b4efb7295ef60dc97470eae0eead901d2
Hongwei <gmhwxi@gmail.com>
2026-07-01 07:09:13 -0400

    Updating: very very minorly

---
+++ contrib/hwxi000/pground/proj001@260624/UTIL/ats3_pydev_main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+########################################################################
+########################################################################
+#
+# HX-2026-06-07:
+# Sun Jun  7 12:10:48 PM EDT 2026
+#
+########################################################################
+########################################################################
+import os
+import sys
+########################################################################
+########################################################################
+sys.setrecursionlimit(66000)
+########################################################################
+########################################################################
+from \
+ats3_pydev_lib2xatsopt \
+import \
+PYDEV_d3parsed_pytrcpy, \
+PYDEV_xatsopt_args_fildats_d3parsdx
+########################################################################
+from \
+DATS.CATS.dynexp3_xprint \
+import PY000_d3parsed_xprint
+########################################################################
+########################################################################
+#
+if __name__ == "__main__":
+    argv = sys.argv
+    # print\
+    # ("ats3_pydev: argv =", argv)
+    def farg(work):
+        for xarg in argv: work(xarg)
+    if len(argv) <= 1:
+        print("Usage: " + \
+        "python3 ats3_pydev_main.py input.dats")
+    if len(argv) >= 2:
+        fpth = sys.argv[1]
+#
+#       d2parsed = \
+#       PYDEV_xatsopt_args_fildats_d2parsed(farg, fpth)
+#       PY_d2parsed = PYDEV_d2parsed_pytrcpy(d2parsed)
+#       print("ats3_pydev: PY_d2parsed =", PY_d2parsed)
+#
+#       d3parsed = \
+#       PYDEV_xatsopt_args_fildats_d3parsed(farg, fpth)
+#       PY_d3parsed = PYDEV_d3parsed_pytrcpy(d3parsed)
+#       print("ats3_pydev: PY_d3parsed =", PY_d3parsed)        
+#
+        d3parsdx = \
+        PYDEV_xatsopt_args_fildats_d3parsdx(farg, fpth)
+        PYDEV_d3parsdx = PYDEV_d3parsed_pytrcpy(d3parsdx)
+        # print("ats3_pydev: PYDEV_d3parsdx =", PYDEV_d3parsdx)
+#
+        PY000_d3parsed_xprint(PYDEV_d3parsdx)
+#
+########################################################################
+########################################################################
+# end of
+# [ATS3-PYDEV/contrib/hwxi000/pground/proj001@260624/UTIL/ats3_pydev_main.py]
+########################################################################
+########################################################################
